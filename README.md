e00cf185af931e83a212c7d22384c6a0bd69d956
github username <github email address>
2019-03-08 14:44:15 -0500

    created model tests for user

---
+++ README.md
@@ -0,0 +1,18 @@
+1. Write the test
+2. Make the test fail/error out
+3. Write the simplest code that could make the test pass.
+4. Refactor to improve.
+
+Store Application
+User
+first name
+last name
+email
+
+Cart
+user_id
+status
+
+Item
+name
+inventory
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,46 @@
+require_relative '../spec_helper'
+
+# close the test for our various models
+# User is the class that we want
+describe User do# describe - wrape and enclose the various test for our various models
+
+  #1. describe the attributes of a user
+  #2. the string '' is ->'use string to describe this paticular test'
+  it 'has a first name, last name, and email' do
+    user = User.new(first_name: "Cernan", last_name: "Bernardo", email: "cernan@flatironschool.com")
+
+    expect(user).to be_valid
+  end
+
+  it 'is invalid without a first name' do
+    user = User.create(last_name: "Bernardo", email: "cernan@flatironschool.com")
+
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid without a last name' do
+    user = User.create(first_name: "Cernan", email: "cernan@flatironschool.com")
+
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid without an email' do
+    user = User.create(last_name: "Bernardo", first_name: "Cernan")
+
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid with a duplicate email' do
+      user1 = User.create(first_name: "Cernan", last_name: "Bernardo", email: "cernan@flatironschool.com")
+      user2 = User.create(first_name: "Ashley", last_name: "Jones", email: "cernan@flatironschool.com")
+
+      expect(user2).to_not be_valid
+  end
+
+  it 'knows its full name' do
+      user = User.create(first_name: "Cernan", last_name: "Bernardo", email: "cernan@flatironschool.com")
+
+      expect(user.full_name).to eq("Cernan Bernardo")
+  end
+
+end
